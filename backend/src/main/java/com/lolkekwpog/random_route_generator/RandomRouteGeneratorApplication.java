20158040241d35ec9eac4a7e9f9b462b1f6ee281
KrutoiChelick <61933733+cchuvakk@users.noreply.github.com>
2025-03-31 23:05:13 +0300

    Initial Commit

---
+++ backend/src/main/java/com/lolkekwpog/random_route_generator/RandomRouteGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.lolkekwpog.random_route_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomRouteGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomRouteGeneratorApplication.class, args);
+	}
+
+}
