313e000930a003b414aaae41efd2fcf15d4b42a0
kol4egraf0-0 <genitya408@gmail.com>
2025-04-02 22:49:40 +0300

    initial pageAuth

---
+++ frontend/src/app/authpage/authpage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-authpage',
+  imports: [],
+  templateUrl: './authpage.component.html',
+  styleUrl: './authpage.component.css'
+})
+export class AuthpageComponent {
+
+}
