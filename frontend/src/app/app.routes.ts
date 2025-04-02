30f5023ba37b4abc50aa508ecaae78cb5ec23829
kol4egraf0-0 <genitya408@gmail.com>
2025-04-02 23:51:29 +0300

    auth init maybe try tommorow

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AuthpageComponent } from './authpage/authpage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    /*{
+        path: 'authpage',
+        component: AuthpageComponent
+    }*/
+];
+//мб чтоб инста открылась!!!
