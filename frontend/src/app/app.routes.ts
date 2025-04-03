9d26fd960529a093e49adb6694678c79134c4d77
kol4egraf0-0 <genitya408@gmail.com>
2025-04-04 00:00:12 +0300

    front na4alo

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { AuthpageComponent } from './authpage/authpage.component';
+import { WelcomepageComponent } from './welcomepage/welcomepage.component';
 
-export const routes: Routes = [
-    /*{
-        path: 'authpage',
-        component: AuthpageComponent
-    }*/
+export const routes: Routes = [ 
+        {path:'', redirectTo:'welcomepage', pathMatch:'full'},
+        {path: 'welcomepage', component: WelcomepageComponent},
+        {path: 'authpage', component: AuthpageComponent},
+
 ];
-//мб чтоб инста открылась!!!
+
