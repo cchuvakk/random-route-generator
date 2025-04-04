21f9fd2b765e8308d5d25102859558f53afa05ca
kol4egraf0-0 <genitya408@gmail.com>
2025-04-04 22:41:13 +0300

    route upd

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,18 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { AuthpageComponent } from './authpage/authpage.component';
 import { WelcomepageComponent } from './welcomepage/welcomepage.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [ 
-        {path:'', redirectTo:'welcomepage', pathMatch:'full'},
+        {path:'', redirectTo:'/welcomepage', pathMatch:'full'},
         {path: 'welcomepage', component: WelcomepageComponent},
         {path: 'authpage', component: AuthpageComponent},
 
 ];
+
+@NgModule({
+        imports:[RouterModule.forRoot(routes)],
+        exports: [RouterModule]
+})
+export class AppRoutingModule{}
 
