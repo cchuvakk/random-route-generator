22e7cf317f208e923d4bfc0fbf7fc04396357702
kol4egraf0-0 <genitya408@gmail.com>
2025-04-12 23:29:13 +0300

    login firebase init

--- frontend/src/app/welcomepage/welcomepage.component.ts
+++ frontend/src/app/welcomepage/welcomepage.component.ts
@@ -1,20 +1,31 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { LoginService } from '../../services/login_service';
 
 @Component({
   selector: 'app-welcomepage',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './welcomepage.component.html',
   styleUrl: './welcomepage.component.css'
 })
 export class WelcomepageComponent {
-  user: string = '';
+  email: string = '';
   password: string = '';
   
-
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+    private loginService: LoginService
+  ) {}
 
   onLogin(){
+    this.loginService.login(this.email, this.password).then(() => {
+      this.router.navigate(['/authpage']); //изменить на main
+    }).catch((error) => {
+      console.error('Ошибка входа:', error);
+      alert('Неверный email или пароль');
+    });
   }
   navigateToRegister() {
     this.router.navigate(['/authpage']); 
