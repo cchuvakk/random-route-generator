1dd1dd7c1ea1fe72ea6a388f0fa3ffdc53cb132b
kol4egraf0-0 <genitya408@gmail.com>
2025-04-15 18:49:32 +0300

    fix bugs w body html

--- frontend/src/app/pages/mainpage/mainpage.component.ts
+++ frontend/src/app/pages/mainpage/mainpage.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-mainpage',
+  standalone: true,
   imports: [],
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.css'
 })
 export class MainpageComponent {
+  sidebarVisible = false;
+
+  toggleSidebar() {
+    this.sidebarVisible = !this.sidebarVisible;
+  }
 
 }
