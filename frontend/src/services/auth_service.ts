00e93bfde920d0827596bd38dddfa718754520dc
kol4egraf0-0 <genitya408@gmail.com>
2025-04-16 18:11:10 +0300

    we 2 typices i hope project didnt bugged

--- frontend/src/services/auth_service.ts
+++ frontend/src/services/auth_service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from "@angular/core";
 import { FirebaseService } from "./firebase_service";
 import {createUserWithEmailAndPassword} from "firebase/auth"
+import { HttpClient } from "@angular/common/http";
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
-    constructor(private firebase: FirebaseService){}
+    constructor(private firebase: FirebaseService, private http:HttpClient){}
 
     register(email: string, password:string): Promise<any> {
         const auth = this.firebase.auth;
-        return createUserWithEmailAndPassword(auth, email, password);
-    }
+        return createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+
+            return this.http.post('http://localhost:8080/api/users', {
+                uid: user.uid,
+                email: user.email
+              }).toPromise();
+         });
+     }
 }
