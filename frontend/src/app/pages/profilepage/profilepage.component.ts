43ad89e22b2dfad84b096515b032cde1b5466cf1
kol4egraf0-0 <genitya408@gmail.com>
2025-04-18 09:51:21 +0300

    logic for profile!

--- frontend/src/app/pages/profilepage/profilepage.component.ts
+++ frontend/src/app/pages/profilepage/profilepage.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-
+import { getAuth } from 'firebase/auth';
 
 @Component({
   selector: 'app-profilepage',
+  standalone: true,
   imports: [],
   templateUrl: './profilepage.component.html',
   styleUrl: './profilepage.component.css'
 })
 export class ProfilepageComponent {
+  userEmail : string = '';
 
+  constructor(){
+    const auth = getAuth();
+    const user = auth.currentUser;
+    this.userEmail = user?.email ?? 'Не зареган!';
+  }
 }
