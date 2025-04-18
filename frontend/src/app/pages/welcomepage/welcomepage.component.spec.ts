b4d038232ee2b444557b269e44998038eaa48b1d
kol4egraf0-0 <genitya408@gmail.com>
2025-04-18 18:30:13 +0300

    test init w/o chrome gg

--- frontend/src/app/pages/welcomepage/welcomepage.component.spec.ts
+++ frontend/src/app/pages/welcomepage/welcomepage.component.spec.ts
@@ -1,23 +1,46 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { WelcomepageComponent } from './welcomepage.component';
+import { Router } from '@angular/router';
+import { LoginService } from '../../../services/login_service';
+import { error } from 'console';
+import { ToasterService } from '../../toaster/toaster.service'; //ng test
 
-describe('WelcomepageComponent', () => {
+fdescribe('WelcomepageComponent - Check Router', () => { //групировка тестов, fdescribe для ток его запуска
   let component: WelcomepageComponent;
-  let fixture: ComponentFixture<WelcomepageComponent>;
+  let fixture: ComponentFixture<WelcomepageComponent> //п
+  let routerSpy: jasmine.SpyObj<Router> 
+  let loginServiceSpy: jasmine.SpyObj<LoginService> 
+  let toasterSpy: jasmine.SpyObj<ToasterService> 
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [WelcomepageComponent]
-    })
-    .compileComponents();
+  beforeEach(async ()=>{
+    routerSpy = jasmine.createSpyObj('Router',['navigate']);
+    loginServiceSpy = jasmine.createSpyObj('LoginService', ['login']);
+    toasterSpy = jasmine.createSpyObj('ToasterService', ['success', 'error']);
+
+    await TestBed.configureTestingModule({ //апишка для тестов
+      declarations: [WelcomepageComponent],
+      providers: [
+        {provide: Router, useValue: routerSpy},
+        {provide: LoginService, useValue: loginServiceSpy},
+        {provide: ToasterService, useValue: toasterSpy}
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(WelcomepageComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('на /mainpage идет ли если чо залогнилися', async ()=>{ //сам тест
+    component.email = 'poggers1337@gmail.com'
+    component.password = '1234567'
+    component.rememberMe = false;
+
+    loginServiceSpy.login.and.returnValue(Promise.resolve());
+
+    //вызов 
+    await component.onLogin();
+
+    //проверочка
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['/mainpage']);
   });
 });
